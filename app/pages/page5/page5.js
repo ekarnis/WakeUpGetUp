db4098bc4be39e75518abd041d058bb914303538
Eric Karnis <ekarnis@outlook.com>
2016-04-09 02:05:29 -0400

    added logout

--- app/pages/page5/page5.js
+++ app/pages/page5/page5.js
@@ -1,11 +1,19 @@
-import {Page} from 'ionic-angular';
-
+import {Page, NavController} from 'ionic-angular';
+import {Login} from '../login/login';
 
 @Page({
   templateUrl: 'build/pages/page5/page5.html'
 })
 export class Page5 {
-  constructor() {
+  static get parameters() {
+    return [[NavController]];
+  }
 
+  constructor(nav) {
+    this.nav = nav;
+  }
+  logOut(){
+    this.nav.setRoot(Login);
+    this.nav.push(Login)
   }
 }
